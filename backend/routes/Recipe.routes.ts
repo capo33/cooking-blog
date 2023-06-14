afb4bedfc78ab513b74e9bdf48a2e622217eb3c0
Mohamed <s2000588@edu.bc.fi>
2023-06-14 18:29:08 +0300

    add like & un like functionality

--- backend/routes/Recipe.routes.ts
+++ backend/routes/Recipe.routes.ts
@@ -19,16 +19,18 @@ router
   // .put(protect, recipeController.saveRecipe)
   // .put(protect, recipeController.unsaveRecipe)
   .post(protect, recipeController.createRecipe);
-router.put("/saveRecipe", protect, recipeController.saveRecipe);
-router.put("/unsaveRecipe", protect, recipeController.unsaveRecipe);
-router
-  .route("/:recipeId")
-  .get(recipeController.getRecipeById)
-  .put(protect, recipeController.updateRecipe)
-  .delete(protect, recipeController.deleteRecipe);
+// router.put("/saveRecipe", protect, recipeController.saveRecipe);
+// router.put("/unsaveRecipe", protect, recipeController.unsaveRecipe);
+router.put("/like", protect, recipeController.likeRecipe);
+router.put("/unlike", protect, recipeController.unlikeRecipe);
+// router
+//   .route("/:recipeId")
+//   .get(recipeController.getRecipeById)
+//   .put(protect, recipeController.updateRecipe)
+//   .delete(protect, recipeController.deleteRecipe);
 
-router.post("/:id/reviews", protect, recipeController.addReview);
-router.get("/savedRecipes/:id", recipeController.getRecipesByUser);
-router.get("/savedRecipes/ids/:id", recipeController.getSavedRecipes);
+// router.post("/:id/reviews", protect, recipeController.addReview);
+// router.get("/savedRecipes/:id", recipeController.getRecipesByUser);
+// router.get("/savedRecipes/ids/:id", recipeController.getSavedRecipes);
 
 export default router;
