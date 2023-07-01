170162111b43a29c62b05d53842e1e5921d2975e
Mohamed <s2000588@edu.bc.fi>
2023-07-01 09:44:09 +0300

    update the navbar

--- frontend/src/components/ComplexNavbar.tsx
+++ frontend/src/components/ComplexNavbar.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Navbar,
   MobileNav,
-  Typography,
   Button,
   Menu,
   MenuHandler,
@@ -12,44 +11,96 @@ import {
   IconButton,
 } from "@material-tailwind/react";
 import {
-  CubeTransparentIcon,
-  UserCircleIcon,
-  CodeBracketSquareIcon,
+  PlusIcon,
+  UserIcon,
+   HomeIcon,
   ChevronDownIcon,
-  Cog6ToothIcon,
-  InboxArrowDownIcon,
-  LifebuoyIcon,
   PowerIcon,
+  ArrowRightOnRectangleIcon,
   Bars2Icon,
+  BookmarkIcon,
 } from "@heroicons/react/24/outline";
+import { logout, userProfile } from "../redux/feature/Auth/authSlice";
+import { useAppDispatch, useAppSelector } from "../redux/app/store";
+import { useNavigate, Link } from "react-router-dom";
 
+interface IProfileMenuItems {
+  label: string;
+  icon: React.ForwardRefExoticComponent<
+    Omit<
+      React.SVGProps<SVGSVGElement> & { title?: string | undefined },
+      "title"
+    > &
+      React.RefAttributes<SVGSVGElement>
+  >;
+  link: string;
+  onClick?: () => void;
+}
 // profile menu component
-const profileMenuItems = [
-  {
-    label: "My Profile",
-    icon: UserCircleIcon,
-  },
-  {
-    label: "Edit Profile",
-    icon: Cog6ToothIcon,
-  },
-  {
-    label: "Inbox",
-    icon: InboxArrowDownIcon,
-  },
-  {
-    label: "Help",
-    icon: LifebuoyIcon,
-  },
-  {
-    label: "Sign Out",
-    icon: PowerIcon,
-  },
-];
 
 function ProfileMenu() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const closeMenu = () => setIsMenuOpen(false);
+
+  const { user } = useAppSelector((state) => state.auth);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const avatar = user?.result?.avatar as string;
+
+  const token = user?.token as string;
+  const admin = user?.result?.isAdmin as boolean;
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+  const profileMenuItems: IProfileMenuItems[] = [
+    {
+      label: "My Profile",
+      icon: UserIcon,
+      link: "/profile",
+    },
+    {
+      label: "Add Recipe",
+      icon: PlusIcon,
+      link: "/add-recipe",
+    },
+    ...(admin
+      ? [
+          {
+            label: "Add Category",
+            icon: PlusIcon,
+            link: "/add-category",
+          },
+        ]
+      : []),
+
+    {
+      label: "Sign Out",
+      icon: PowerIcon,
+      link: "/login",
+      onClick: handleLogout,
+    },
+  ];
+
+  const authMenuItems: IProfileMenuItems[] = [
+    {
+      label: "Login",
+      icon: ArrowRightOnRectangleIcon,
+      link: "/login",
+    },
+    {
+      label: "Register",
+      icon: ArrowRightOnRectangleIcon,
+      link: "/register",
+    },
+  ];
+
+  useEffect(() => {
+    if (token) dispatch(userProfile(token));
+  }, [dispatch, token]);
 
   return (
     <Menu open={isMenuOpen} handler={setIsMenuOpen} placement='bottom-end'>
@@ -63,8 +114,12 @@ function ProfileMenu() {
             variant='circular'
             size='sm'
             alt='candice wu'
-            className='border border-blue-500 p-0.5'
-            src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80'
+            className='p-0.5'
+            src={
+              avatar
+                ? avatar
+                : "https://t4.ftcdn.net/jpg/03/32/59/65/240_F_332596535_lAdLhf6KzbW6PWXBWeIFTovTii1drkbT.jpg"
+            }
           />
           <ChevronDownIcon
             strokeWidth={2.5}
@@ -75,33 +130,61 @@ function ProfileMenu() {
         </Button>
       </MenuHandler>
       <MenuList className='p-1'>
-        {profileMenuItems.map(({ label, icon }, key) => {
-          const isLastItem = key === profileMenuItems.length - 1;
-          return (
-            <MenuItem
-              key={label}
-              onClick={closeMenu}
-              className={`flex items-center gap-2 rounded ${
-                isLastItem
-                  ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                  : ""
-              }`}
-            >
-              {React.createElement(icon, {
-                className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
-                strokeWidth: 2,
-              })}
-              <Typography
-                as='span'
-                variant='small'
-                className='font-normal'
-                color={isLastItem ? "red" : "inherit"}
-              >
-                {label}
-              </Typography>
-            </MenuItem>
-          );
-        })}
+        {user
+          ? profileMenuItems.map(({ label, link, icon, onClick }, key) => {
+              const isLastItem = key === profileMenuItems.length - 1;
+              return (
+                <MenuItem
+                  key={label}
+                  onClick={closeMenu}
+                  className={`flex items-center gap-2 rounded ${
+                    isLastItem
+                      ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                      : ""
+                  }`}
+                >
+                  {React.createElement(icon, {
+                    className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                    strokeWidth: 2,
+                  })}
+                  <Link
+                    to={link}
+                    onClick={link === "/login" ? onClick : undefined}
+                    className='font-normal'
+                    color={isLastItem ? "red" : "inherit"}
+                  >
+                    {label}
+                  </Link>
+                </MenuItem>
+              );
+            })
+          : authMenuItems.map(({ label, link, icon, onClick }, key) => {
+              const isLastItem = key === authMenuItems.length - 1;
+              return (
+                <MenuItem
+                  key={label}
+                  onClick={closeMenu}
+                  className={`flex items-center gap-2 rounded ${
+                    isLastItem
+                      ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+                      : ""
+                  }`}
+                >
+                  {React.createElement(icon, {
+                    className: `h-4 w-4 ${isLastItem ? "text-red-500" : ""}`,
+                    strokeWidth: 2,
+                  })}
+                  <Link
+                    to={link}
+                    onClick={link === "/login" ? onClick : undefined}
+                    className='font-normal'
+                    color={isLastItem ? "red" : "inherit"}
+                  >
+                    {label}
+                  </Link>
+                </MenuItem>
+              );
+            })}
       </MenuList>
     </Menu>
   );
@@ -109,39 +192,38 @@ function ProfileMenu() {
 
 // nav list menu
 
-// nav list component
-const navListItems = [
-  {
-    label: "Account",
-    icon: UserCircleIcon,
-  },
-  {
-    label: "Blocks",
-    icon: CubeTransparentIcon,
-  },
-  {
-    label: "Docs",
-    icon: CodeBracketSquareIcon,
-  },
-];
-
 function NavList() {
+  const { user } = useAppSelector((state) => state.auth);
+
+  const token = user?.token as string;
+
+  // nav list component
+  const navListItems = [
+    {
+      label: "Recipes",
+      icon: HomeIcon,
+      link: "/",
+    },
+    ...(token
+      ? [
+          {
+            label: "Saved Recipes",
+            icon: BookmarkIcon,
+            link: "/saved-recipes",
+          },
+        ]
+      : []),
+  ];
+
   return (
     <ul className='mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center'>
-      {navListItems.map(({ label, icon }, key) => (
-        <Typography
-          key={label}
-          as='a'
-          href='#'
-          variant='small'
-          color='blue-gray'
-          className='font-normal'
-        >
+      {navListItems.map(({ label, link, icon }, key) => (
+        <Link to={link} color='blue-gray' className='font-normal' key={label}>
           <MenuItem className='flex items-center gap-2 lg:rounded-full'>
             {React.createElement(icon, { className: "h-[18px] w-[18px]" })}{" "}
             {label}
           </MenuItem>
-        </Typography>
+        </Link>
       ))}
     </ul>
   );
@@ -161,13 +243,9 @@ export default function ComplexNavbar() {
   return (
     <Navbar className='mx-auto max-w-screen-xl p-2 lg:rounded-full lg:pl-6'>
       <div className='relative mx-auto flex items-center text-blue-gray-900'>
-        <Typography
-          as='a'
-          href='#'
-          className='mr-4 ml-2 cursor-pointer py-1.5 font-medium'
-        >
+        <Link to='/' className='mr-4 ml-2 cursor-pointer py-1.5 font-medium'>
           Material Tailwind
-        </Typography>
+        </Link>
         <div className='absolute top-2/4 left-2/4 hidden -translate-x-2/4 -translate-y-2/4 lg:block'>
           <NavList />
         </div>
